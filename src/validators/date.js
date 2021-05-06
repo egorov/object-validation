0d366767ff78d06f09da7e9471eb5d7e3d989959
Sergey Egorov <s.e.888@live.ru>
2021-05-06 11:10:17 +0500

    String Date representation validation has been added

--- src/validators/date.js
+++ src/validators/date.js
@@ -4,7 +4,21 @@ module.exports = function date(value) {
     type: 'date'
   };
 
-  if(!(value instanceof Date)) return error;
+  if(value instanceof Date) {
+
+    if(Number.isNaN(value.valueOf()))
+      return error;
+
+    return null;    
+  }
+
+  if(typeof value !== 'string') 
+    return error;
   
+  const result = new Date(value);
+
+  if(Number.isNaN(result.valueOf()))
+    return error;
+ 
   return null;
 }
--- tests/validators/date_spec.js
+++ tests/validators/date_spec.js
@@ -5,6 +5,7 @@ describe('date', () => {
   it('should return null', () => {
 
     expect(validate(new Date())).toBeNull();
+    expect(validate('2021-04-30T10:00:00.000Z')).toBeNull();
   });
 
   it('should return error', () => {
@@ -20,5 +21,8 @@ describe('date', () => {
     expect(validate({})).toEqual(error);
     expect(validate([])).toEqual(error);
     expect(validate('not date')).toEqual(error);
+    expect(validate('2021-35-38')).toEqual(error);
+    expect(validate('2021-14-41T78:00:00.000Z')).toEqual(error);
+    expect(validate(new Date('2021-14-41T78:00:00.000Z'))).toEqual(error);
   });
 });
